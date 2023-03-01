dfbc33cc73eb644357db8ff58f08d9ffed262061
chunglh <lhchung306@gmail.com>
2023-03-01 12:10:12 +0700

    load songs

--- app/src/main/java/com/example/project_prm/MainActivity.java
+++ app/src/main/java/com/example/project_prm/MainActivity.java
@@ -2,13 +2,85 @@ package com.example.project_prm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
 import android.os.Bundle;
+import android.os.Debug;
+import android.os.Environment;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+    String[] items;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView = findViewById(R.id.listViewSong);
+        runTimePermission();
+    }
+
+    public void runTimePermission() {
+        Dexter.withContext(this).withPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                .withListener(new PermissionListener() {
+                    @Override
+                    public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+                        displaySong();
+                    }
+
+                    @Override
+                    public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
+
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
+                        permissionToken.continuePermissionRequest();
+                    }
+                }).check();
+    }
+
+    public ArrayList<File> findSong(File file) {
+        ArrayList<File> list = new ArrayList<>();
+        File[] files = file.listFiles();
+        if(files != null) {
+            for (File aFile: files) {
+                if (aFile.isDirectory() && !aFile.isHidden()) {
+                    list.addAll(findSong(aFile));
+                } else {
+                    if (aFile.getName().endsWith(".mp3")
+                            || aFile.getName().endsWith(".wav")
+                    ) {
+                        list.add(aFile);
+                    }
+                }
+            }
+        }
+        return list;
+    }
+
+    public void displaySong() {
+        final ArrayList<File> songs = findSong(Environment.getExternalStorageDirectory());
+        items = new String[songs.size()];
+        for (int i = 0; i < songs.size(); i++) {
+            items[i] = songs.get(i).getName().toString().replace(".mp3", "").replace(".wav", "");
+        }
+        ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items) ;
+        listView.setAdapter(myAdapter);
     }
 }
